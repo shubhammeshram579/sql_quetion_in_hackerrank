c425451b16c2ec8adbef8d800e35501dab5fd6b0
Shubham Meshram <105120972+shubhammeshram579@users.noreply.github.com>
2023-10-26 14:09:03 +0530

    Update 1.Averange_population.sql

--- 1.Averange_population.sql
+++ 1.Averange_population.sql
@@ -141,3 +141,24 @@ from employee
 group by months * salary
 order by months * salary desc
 limit 1;
+
+
+
+
+
+/*Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.
+
+Input Format
+
+The STATION table is described as follows:
+
+Station.jpg
+
+where LAT_N is the northern latitude and LONG_W is the western longitude.*/
+
+-- select * from Station
+
+select distinct city
+from station
+where lower(substr(city,1,1)) not in ('a','e','i','o','u')
+or lower(substr(city,length(city),1)) not in ('a','e','i','o');
